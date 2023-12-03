1ac273ec49c902960e416505932a3e148e6289f6
pyalexos <aligos@yandex.ru>
2023-12-03 14:54:36 +0300

    kk

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# SFEDU_telebot
+# Captain_telebot
 Telegram bot based on the aiogram library. Provides data on requests received from keyboard buttons. Messages are stored in .html format.
 
 There is a section with inline buttons. Functions are included to get the file id and photo in the chat for its further forwarding to other users.
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from aiogram.types import Message
+
 from API_TOKEN import API_TOKEN
 import texts
 
@@ -5,7 +7,7 @@ import asyncio
 import logging
 
 from aiogram import Bot, Dispatcher, types, F
-from aiogram.filters import Command, Text
+from aiogram.filters import Command
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 logging.basicConfig(level=logging.INFO)
@@ -13,41 +15,8 @@ bot = Bot(token=API_TOKEN, parse_mode="HTML")
 dp = Dispatcher()
 
 
-@dp.message(Command('start', 'role'))
+@dp.message(Command('start', 'menu'))
 async def start(message: types.Message):
-    if message.text == '/start':
-        await message.answer(texts.hello)
-
-    keyboard = [[
-        types.KeyboardButton(text='Для абитуриента 🎒'),
-        types.KeyboardButton(text='Для студента 🎓')
-    ]]
-
-    reply_markup = types.ReplyKeyboardMarkup(keyboard=keyboard, resize_keyboard=True,
-                                             input_field_placeholder='Подскажи, кто ты?')
-    await message.answer('Какую информацию ты хочешь видеть?', reply_markup=reply_markup)
-
-
-@dp.message(Text('Для абитуриента 🎒'))
-async def menu_abit(message: types.Message):
-    keyboard = [
-        [
-            types.KeyboardButton(text='Поступление 📄'),
-            types.KeyboardButton(text='Общежития 🏠'),
-        ],
-        [
-            types.KeyboardButton(text='Направления подготовки 👉'),
-            types.KeyboardButton(text='Контакты приёмной комиссии ☎️'),
-        ]
-    ]
-
-    reply_markup = types.ReplyKeyboardMarkup(keyboard=keyboard, resize_keyboard=True, one_time_keyboard=True,
-                                             input_field_placeholder='Выбери нужный раздел')
-    await message.answer(texts.okay, reply_markup=reply_markup)
-
-
-@dp.message(Text('Для студента 🎓'))
-async def menu_stud(message: types.Message):
     keyboard = [
         [
             types.KeyboardButton(text='Стипендии 💰'),
@@ -58,8 +27,11 @@ async def menu_stud(message: types.Message):
             types.KeyboardButton(text='Центр карьеры 🔍'),
         ],
         [
-            types.KeyboardButton(text='Личный кабинет 🔑'),
+            types.KeyboardButton(text='Домашнее задание ✏️'),
             types.KeyboardButton(text='Запись на ФЗК 🏀'),
+        ],
+        [
+            types.KeyboardButton(text='Психологический клуб «Лифт» 🚀'),
         ],
         [
             types.KeyboardButton(text='Объединённый совет учащихся 👥💬'),
@@ -68,160 +40,91 @@ async def menu_stud(message: types.Message):
 
     reply_markup = types.ReplyKeyboardMarkup(keyboard=keyboard, resize_keyboard=True, one_time_keyboard=True,
                                              input_field_placeholder='Выбери нужный раздел')
-    await message.answer(texts.okay, reply_markup=reply_markup)
+    if message.text == '/start':
+        await message.answer(texts.hello + "\n" + texts.action, reply_markup=reply_markup)
+    else:
+        await message.answer(texts.action, reply_markup=reply_markup)
 
 
-@dp.message(Text('Поступление 📄'))
-@dp.message(Text('Общежития 🏠'))
-@dp.message(Text('Направления подготовки 👉'))
-@dp.message(Text('Контакты приёмной комиссии ☎️'))
-async def abit(message: types.Message):
-    text = ''
-    keyboard = [[types.InlineKeyboardButton(text='« Назад', callback_data='menu_abit')]]
-    reply_markup = types.InlineKeyboardMarkup(inline_keyboard=keyboard, resize_keyboard=True, one_time_keyboard=True)
-
-    if message.text == 'Направления подготовки 👉':
-        text = texts.directions
-
-    if message.text == 'Общежития 🏠':
-        text = texts.hostel
-
-    elif message.text == 'Поступление 📄':
-        text = texts.entrance
-
-    elif message.text == 'Контакты приёмной комиссии ☎️':
-        text = texts.comission
-
-    await message.answer(text=text, reply_markup=reply_markup)
-
-
-@dp.message(Text('Переход с платки 📚'))
-@dp.message(Text('Студпрофком 💼'))
-@dp.message(Text('Центр карьеры 🔍'))
-@dp.message(Text('Личный кабинет 🔑'))
-@dp.message(Text('Запись на ФЗК 🏀'))
-@dp.message(Text('Объединённый совет учащихся 👥💬'))
+@dp.message(F.text)
 async def stud(message: types.Message):
     keyboard = [[types.InlineKeyboardButton(text='« Назад', callback_data='menu_stud')]]
     reply_markup = types.InlineKeyboardMarkup(inline_keyboard=keyboard, resize_keyboard=True, one_time_keyboard=True)
 
     if message.text == 'Переход с платки 📚':
-        text = texts.become_free
-        file = 'BQACAgIAAxkBAAIFOGRhRWXeQ-DM_rFLLxly6iGHoksbAAJ-MAACzgwJS7KAwbbVesiqLwQ'
-        await message.answer_document(file)
-        await message.answer(text=text, reply_markup=reply_markup)
+        await message.answer(text=texts.become_free, reply_markup=reply_markup)
 
     elif message.text == 'Студпрофком 💼':
-        text = texts.studprofcom
-        await message.answer(text=text, reply_markup=reply_markup)
+        await message.answer(text=texts.studprofcom, reply_markup=reply_markup)
 
     elif message.text == 'Центр карьеры 🔍':
-        text = texts.career
-        await message.answer(text=text, reply_markup=reply_markup)
+        await message.answer(text=texts.career, reply_markup=reply_markup)
 
-    elif message.text == 'Личный кабинет 🔑':
-        text = texts.lk
-        photos = [
-            types.InputMediaPhoto(
-                media='AgACAgIAAxkBAAIFVWRhR0e4w2zYB14ZfJkkA5sYT1IYAAIczTEbzgwJS9dKTqpYROf0AQADAgADeQADLwQ'),
-            types.InputMediaPhoto(
-                media='AgACAgIAAxkBAAIFVmRhR04al8KY1HMkzyiIiaWnUmAjAAIdzTEbzgwJSxWTCRhseLmRAQADAgADeQADLwQ')
-        ]
-
-        await message.answer_media_group(photos)
-        await message.answer(text=text, reply_markup=reply_markup)
+    elif message.text == 'Домашнее задание ✏️':
+        await message.answer(text=texts.homework, reply_markup=reply_markup)
 
     elif message.text == 'Запись на ФЗК 🏀':
-        text = texts.phys_ed
-        photo = 'AgACAgIAAxkBAAIFV2RhR1Lr-Mk5AAF3SLjBPwgXZCwj5wACHs0xG84MCUsQpYTDJJyyUAEAAwIAA3kAAy8E'
-        await message.answer_photo(photo=photo, caption=text, reply_markup=reply_markup)
+        await message.answer(text=texts.phys_ed, reply_markup=reply_markup)
+
+    elif message.text == 'Психологический клуб «Лифт» 🚀':
+        await message.answer(text=texts.education, reply_markup=reply_markup)
 
     elif message.text == 'Объединённый совет учащихся 👥💬':
-        text = texts.oso
-        await message.answer(text=text, reply_markup=reply_markup)
+        await message.answer(text=texts.oso, reply_markup=reply_markup)
+
+    else:
+        keyboard = [
+            [types.InlineKeyboardButton(text='Академическая стипендия', callback_data='st_acad')],
+            [types.InlineKeyboardButton(text='Повышенная академическая стипендия', callback_data='st_acad_pow')],
+            [types.InlineKeyboardButton(text='Государственная социальная стипендия', callback_data='st_gov')],
+            [types.InlineKeyboardButton(text='Государственная социальная стипендия 1-2 курсов (стимулирующая выплата)',
+                                        callback_data='st_gov_plus')],
+            [types.InlineKeyboardButton(text='Стипендии президента и правительства РФ', callback_data='st_prez')],
+            [types.InlineKeyboardButton(text='Именные стипендии', callback_data='st_named')],
+            [types.InlineKeyboardButton(text='« Назад', callback_data='menu_stud')],
+        ]
+
+        reply_markup = types.InlineKeyboardMarkup(inline_keyboard=keyboard, resize_keyboard=True, one_time_keyboard=True)
+        await message.answer(text='Выбери интересующую тебя стипендию и я о ней расскажу', reply_markup=reply_markup)
 
 
-@dp.message(Text('Стипендии 💰'))
-async def get_money(message: types.Message):
-    keyboard = [
-        [types.InlineKeyboardButton(text='Академическая стипендия', callback_data='st_acad')],
-        [types.InlineKeyboardButton(text='Повышенная академическая стипендия', callback_data='st_acad_pow')],
-        [types.InlineKeyboardButton(text='Государственная социальная стипендия', callback_data='st_gov')],
-        [types.InlineKeyboardButton(text='Государственная социальная стипендия 1-2 курсов (стимулирующая выплата)',
-                                    callback_data='st_gov_plus')],
-        [types.InlineKeyboardButton(text='Стипендии президента и правительства РФ', callback_data='st_prez')],
-        [types.InlineKeyboardButton(text='Именные стипендии', callback_data='st_named')],
-        [types.InlineKeyboardButton(text='« Назад', callback_data='menu_stud')],
-    ]
-
-    reply_markup = types.InlineKeyboardMarkup(inline_keyboard=keyboard, resize_keyboard=True, one_time_keyboard=True)
-    await message.answer(text='Выбери интересующую тебя стипендию и я о ней расскажу', reply_markup=reply_markup)
-
-
-@dp.callback_query(Text("st_acad"))
-@dp.callback_query(Text("st_acad_pow"))
-@dp.callback_query(Text("st_gov"))
-@dp.callback_query(Text("st_gov_plus"))
-@dp.callback_query(Text("st_prez"))
-@dp.callback_query(Text("st_named"))
-async def redirect(callback: types.CallbackQuery):
-    text, header = '', ''
+@dp.callback_query(F.data)
+async def st(callback: types.CallbackQuery):
     keyboard = [[types.InlineKeyboardButton(text='« Назад', callback_data='st')]]
     reply_markup = types.InlineKeyboardMarkup(inline_keyboard=keyboard, resize_keyboard=True, one_time_keyboard=True)
     await callback.answer()
 
     if callback.data == 'st_named':
-        await callback.message.edit_text(text='<b>Именные стипендии</b>', reply_markup=None)
-
-        files = [
-            types.InputMediaDocument(
-                media='BQACAgIAAxkBAAIHbWRhZ2htrznZOSEdaqanv9EdBUSpAALCMAACzgwJS1Yv-LFVQgfaLwQ'),
-            types.InputMediaDocument(
-                media='BQACAgIAAxkBAAIHbmRhZ3RUJIWnzDjFGjihrmZKKq-fAALDMAACzgwJS7Qsh4cC56LJLwQ')
-        ]
-
-        await callback.message.answer_media_group(files)
+        await callback.message.edit_text(text=f'<b>Именные стипендии</b>', reply_markup=None)
         await callback.message.answer(text=texts.st_named, reply_markup=reply_markup)
 
-    else:
-        if callback.data == 'st_acad':
-            header = 'Академическая стипендия'
-            text = texts.st_acad
+    elif callback.data == 'st_acad':
+        await callback.message.edit_text(text=f'<b>Академическая стипендия</b>', reply_markup=None)
+        await callback.message.answer(text=texts.st_acad, reply_markup=reply_markup)
 
-        elif callback.data == 'st_acad_pow':
-            header = 'Повышенная академическая стипендия'
-            text = texts.st_acad_pow
+    elif callback.data == 'st_acad_pow':
+        await callback.message.edit_text(text=f'<b>Повышенная академическая стипендия</b>', reply_markup=None)
+        await callback.message.answer(text=texts.st_acad_pow, reply_markup=reply_markup)
 
-        elif callback.data == 'st_gov':
-            header = 'Государственная социальная стипендия'
-            text = texts.st_gov
+    elif callback.data == 'st_gov':
+        await callback.message.edit_text(text=f'<b>Государственная социальная стипендия</b>', reply_markup=None)
+        await callback.message.answer(text=texts.st_gov, reply_markup=reply_markup)
 
-        elif callback.data == 'st_gov_plus':
-            header = 'Государственная социальная стипендия 1-2 курсов (стимулирующая выплата)'
-            text = texts.st_gov_plus
+    elif callback.data == 'st_gov_plus':
+        await callback.message.edit_text(text=f'<b>Государственная социальная стипендия 1-2 курсов (стимулирующая выплата)</b>', reply_markup=None)
+        await callback.message.answer(text=texts.st_gov_plus, reply_markup=reply_markup)
 
-        elif callback.data == 'st_prez':
-            header = 'Стипендии президента и правительства РФ'
-            text = texts.st_prez
-
-        await callback.message.edit_text(text=f'<b>{header}</b>', reply_markup=None)
-        await callback.message.answer(text=text, reply_markup=reply_markup)
-
-
-@dp.callback_query(Text("menu_stud"))
-@dp.callback_query(Text("menu_abit"))
-@dp.callback_query(Text("st"))
-async def redirect(callback: types.CallbackQuery):
-    await callback.answer()
-    await callback.message.edit_reply_markup(reply_markup=None)
-    if callback.data == 'menu_abit':
-        await menu_abit(callback.message)
+    elif callback.data == 'st_prez':
+        await callback.message.edit_text(text=f'<b>Стипендии президента и правительства РФ</b>', reply_markup=None)
+        await callback.message.answer(text=texts.st_prez, reply_markup=reply_markup)
 
     elif callback.data == 'menu_stud':
-        await menu_stud(callback.message)
+        await callback.message.edit_reply_markup(reply_markup=None)
+        await start(callback.message)
 
     elif callback.data == 'st':
-        await get_money(callback.message)
+        await callback.message.edit_reply_markup(reply_markup=None)
+        await stud(callback.message)
 
 
 """@dp.message(F.photo)
